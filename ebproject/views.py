7c3688fe54952a46caaf86d67d5d5dd07f0661f8
Samuel <smohuff@gmail.com>
2022-08-10 18:44:15 -0500

    home.html has been created and connected, home.html now displays the json request. api get request was fixed with headers.

--- ebproject/views.py
+++ ebproject/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import requests
+
+# temp key: will be a environment variable for production
+key = "l7u502p8v46ba3ppgvj5y2aad50lb9"
 
 # Create your views here.
+
+def home(request):
+    url = "https://api.stagingeb.com/v1/properties"
+    usr = "X-Authorization"
+    req = requests.get(url, headers={usr: key})
+    data = req.json()
+
+    context = {'data' : data}
+
+    return render(request, 'ebproject/home.html', context)
