9dfccf136b39f4311c26eece062009a63c7d4710
Samuel <smohuff@gmail.com>
2022-08-12 01:26:54 -0500

    Added a very basic pagination system urls.py: new 'page' path views.py: pagination logic + page func (almost identical to home) home.html: basic pagination view/system

--- ebproject/urls.py
+++ ebproject/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:value>/', views.page, name='page'),
     path('property/<str:value>/', views.properties, name='property'),
     path('contact/', views.contact, name='contact')
 ]
