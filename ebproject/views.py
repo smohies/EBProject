f7fc7f7c3c80bf32e0891df02d444b46c8fb019c
Samuel <smohuff@gmail.com>
2022-08-12 02:55:43 -0500

    removed pages function as to avoid redundancy pages url now points to home view added parameter with default value in home function

--- ebproject/views.py
+++ ebproject/views.py
@@ -5,33 +5,7 @@ from .forms import InputForm
 
 # Create your views here.
 
-def home(request):
-    url = "https://api.stagingeb.com/v1/properties"
-    usr = "X-Authorization"
-    key = "l7u502p8v46ba3ppgvj5y2aad50lb9"
-    headers = {usr: key}
-    payload = {"page": 1, "limit": 15, "search[statuses][]": "published"}
-    req = requests.get(url, headers=headers, params=payload)
-    data = req.json()
-
-    pagination = data['pagination']
-    pagination["pages"] = pagination["total"] // pagination["limit"]
-    if pagination["total"] % pagination["limit"]:
-        pagination["pages"] += 1
-    pages = [n+1 for n in range(pagination["pages"])]
-
-    content = data['content']
-
-    # add thumbnail to properties with a null thumbnail
-    for count in range(len(content)):
-        if not content[count]["title_image_thumb"]:
-            content[count]["title_image_thumb"] = "https://www.komar.de/en/media/catalog/product/cache/5/image/100x100/17f82f742ffe127f42dca9de82fb58b1/6/0/6041a-vd2_blue_sky_web.jpg"
-
-    context = {'content': content, 'pagination': pagination, 'pages': pages}
-
-    return render(request, 'ebproject/home.html', context)
-
-def page(request, value):
+def home(request, value=1):
     url = "https://api.stagingeb.com/v1/properties"
     usr = "X-Authorization"
     key = "l7u502p8v46ba3ppgvj5y2aad50lb9"
