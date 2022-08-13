845236ea7ed71c222c12ac2ac01316cfdc0f5fef
Samuel <smohuff@gmail.com>
2022-08-13 01:57:21 -0500

    Image slideshow logic passed to html

--- ebproject/views.py
+++ ebproject/views.py
@@ -31,22 +31,8 @@ def properties(request, property_id):
     url = urljoin(settings.EB_BASE_URL, settings.EB_PROPERTIES_ENDPOINT + f"/{property_id}")
     res = requests.get(url, auth=TokenAuth(settings.API_KEY))
     data = res.json()
-
-    # Create a dictionary with the property image urls
-    # if less than 3, repeat until 3, if none, add 3 generic images.
-    images = {}
-    if len(data["property_images"]) > 0:
-        for count, image in enumerate(data["property_images"]):
-            images[count] = image["url"]
-        if 1 not in images:
-            images[1] = images[0]
-        if 2 not in images:
-            images[2] = images[0]
-    else:
-        for count in range(3):
-            images[count] = "http://wallpaperswide.com/download/sky_hd-wallpaper-1920x1080.jpg"
-
-    context = {'data': data, 'images': images,
+    
+    context = {'data': data, 'slider': range(len(data["property_images"])),
                'form': InputForm(initial={'public_id': property_id})}
 
     return render(request, 'ebproject/property.html', context)
